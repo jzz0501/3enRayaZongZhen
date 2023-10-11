d39a20b500b38e9ad05d178c8ea068136384f059
zongzhen <jiangzongzhen51@gmail.com>
2023-10-11 19:35:05 +0100

    my first event

--- 3-en-raya/src/App.js
+++ 3-en-raya/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-export default function Square() {
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square values="1" />
+        <Square values="2" />
+        <Square values="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square values="4" />
+        <Square values="5" />
+        <Square values="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square values="7" />
+        <Square values="8" />
+        <Square values="9" />
       </div>
     </>
+  );
+}
+
+function Square({values}) {
+
+  function handleClick() {
+    console.log('hiciste click!');
+  }
+
+  return (
+    <button 
+      className="square" 
+      onClick={handleClick}
+    >
+      {values}
+    </button>
   );
 }
